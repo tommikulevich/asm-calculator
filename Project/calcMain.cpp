c52f0b8eebdc2cd4b8f396e63e583ac722ba6fcf
Szymon Kryzel <szymonkryzel13@gmail.com>
2023-06-20 00:14:29 +0200

    Cosmetic changes in GUI, BMI calculation

--- Project/calcMain.cpp
+++ Project/calcMain.cpp
@@ -14,83 +14,100 @@ int main()
     char operation;
 
     cout << "Calculator:" << endl;
-    cout << "a = ";
-    cin >> a;
-    cout << "b = ";
-    cin >> b;
-    cout << "Operation (+ - * / % ^): ";
-    cin >> operation;
 
-    switch (operation) {
-    case '+':
-        __asm {
-            mov eax, a
-            add eax, b
-            jc overflow         // check for overflow
-            mov result, eax
-            jmp end_add
-        overflow:
-            mov eax, LONG_MAX
-            mov result, eax
-        end_add: 
-            ;
-        };
-        break;
-    case '-':
-        __asm {
-            mov eax, a
-            sub eax, b
-            jnc underflow       // check for underflow
-            mov result, eax
-            jmp end_sub
-        underflow:
-            mov eax, LONG_MIN
-            mov result, eax
-        end_sub: 
-            ;
-        };
-        break;
-    case '*':
-        __asm {
-            mov eax, a
-            imul eax, b
-            jo mul_overflow     // check for overflow
-            mov result, eax
-            jmp end_mul
-        mul_overflow:
-            mov eax, LONG_MAX
-            mov result, eax
-        end_mul: 
-            ;
-        };
-        break;
-    case '/':
-        if (b == 0) {
-            cout << "Cannot divide by zero!" << endl;
+    while (1) {
+        cout << "a = ";
+        cin >> a;
+        cout << "b = ";
+        cin >> b;
+        cout << "Operation (+ - * / % ^) or b to calculate BMI: ";
+        cin >> operation;
+
+        switch (operation) {
+        case '+':
+            __asm {
+                mov eax, a
+                add eax, b
+                jc overflow         // check for overflow
+                mov result, eax
+                jmp end_add
+                overflow :
+                mov eax, LONG_MAX
+                    mov result, eax
+                    end_add :
+                ;
+            };
+            break;
+        case '-':
+            __asm {
+                mov eax, a
+                sub eax, b
+                jnc underflow       // check for underflow
+                mov result, eax
+                jmp end_sub
+                underflow :
+                mov eax, LONG_MIN
+                    mov result, eax
+                    end_sub :
+                ;
+            };
+            break;
+        case '*':
+            __asm {
+                mov eax, a
+                imul eax, b
+                jo mul_overflow     // check for overflow
+                mov result, eax
+                jmp end_mul
+                mul_overflow :
+                mov eax, LONG_MAX
+                    mov result, eax
+                    end_mul :
+                ;
+            };
+            break;
+        case 'b':
+            __asm {
+                mov eax, a
+
+                mov ebx, b
+                imul ebx, ebx
+
+                xor edx, edx
+                div ebx
+
+                mov result, eax
+            };
+            break;
+        case '/':
+            if (b == 0) {
+                cout << "Cannot divide by zero!" << endl;
+                return 1;
+            }
+            result = divide(a, b);
+            break;
+        case '%':
+            if (b == 0) {
+                cout << "Cannot divide by zero!" << endl;
+                return 1;
+            }
+            result = modulo(a, b);
+            break;
+        case '^':
+            if (b < 0) {
+                cout << "Cannot raise to a negative power!" << endl;
+                return 1;
+            }
+            result = power(a, b);
+            break;
+        default:
+            cout << "Unknown operation!" << endl;
             return 1;
         }
-        result = divide(a, b);
-        break;
-    case '%':
-        if (b == 0) {
-            cout << "Cannot divide by zero!" << endl;
-            return 1;
-        }
-        result = modulo(a, b);
-        break;
-    case '^':
-        if (b < 0) {
-            cout << "Cannot raise to a negative power!" << endl;
-            return 1;
-        }
-        result = power(a, b);
-        break;
-    default:
-        cout << "Unknown operation!" << endl;
-        return 1;
+        if (operation == 'b')   cout << "Weight " << a << " kg, "<< b << " m, your BMI is " << result << endl;
+        else cout << a << " " << operation << " " << b << " = " << result << endl;
+        cout << endl;
     }
-
-    cout << "Result: " << result << endl;
 
     return 0;
 }
